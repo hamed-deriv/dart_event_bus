6e7b8023be217ca74ca81e44c1f53c3d28fcbd48
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2023-02-14 11:49:56 +0800

    add event bus class

--- bin/dart_event_bus.dart
+++ bin/dart_event_bus.dart
@@ -1,1 +1,23 @@
+// ignore_for_file: cascade_invocations
 
+import 'package:dart_event_bus/event_bus.dart';
+
+void main(List<String> args) {
+  final EventBus bus = EventBus();
+
+  final Function stringListener = bus.subscribe<String>(
+    (String event) => print('Received string event: $event'),
+  );
+
+  final Function intListener =
+      bus.subscribe<int>((int event) => print('Received int event: $event'));
+
+  bus.publish('Hello, world!');
+  bus.publish(10);
+
+  bus.unsubscribe<String>(stringListener);
+  bus.unsubscribe<int>(intListener);
+
+  bus.publish('Hello, world!');
+  bus.publish(10);
+}
--- lib/event_bus.dart
+++ lib/event_bus.dart
@@ -1,1 +1,23 @@
+class EventBus {
+  final Map<Type, List<Function>> _listeners = <Type, List<Function>>{};
 
+  void publish<T>(T event) {
+    if (_listeners.containsKey(T)) {
+      _listeners[T]?.forEach((Function listener) => listener(event));
+    }
+  }
+
+  Function subscribe<T>(void Function(T) listener) {
+    if (!_listeners.containsKey(T)) {
+      _listeners[T] = <Function>[];
+    }
+    _listeners[T]?.add(listener);
+    return listener;
+  }
+
+  void unsubscribe<T>(Function listener) {
+    if (_listeners.containsKey(T)) {
+      _listeners[T]?.remove(listener);
+    }
+  }
+}
