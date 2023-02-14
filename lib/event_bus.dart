a15c8e133c7305476f573d98211bf07c16cc690b
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2023-02-14 12:29:56 +0800

    add exception handling

--- lib/event_bus.dart
+++ lib/event_bus.dart
@@ -2,22 +2,41 @@ class EventBus {
   final Map<Type, List<Function>> _listeners = <Type, List<Function>>{};
 
   void publish<T>(T event) {
-    if (_listeners.containsKey(T)) {
-      _listeners[T]?.forEach((Function listener) => listener(event));
+    try {
+      if (_listeners.containsKey(T)) {
+        _listeners[T]?.forEach((Function listener) => listener(event));
+      }
+    } on Exception catch (e) {
+      print('Error publishing $event: $e');
+
+      rethrow;
     }
   }
 
   Function subscribe<T>(void Function(T) listener) {
-    if (!_listeners.containsKey(T)) {
-      _listeners[T] = <Function>[];
+    try {
+      if (!_listeners.containsKey(T)) {
+        _listeners[T] = <Function>[];
+      }
+      _listeners[T]?.add(listener);
+      
+      return listener;
+    } on Exception catch (e) {
+      print('Error subscribing to event: $e');
+
+      rethrow;
     }
-    _listeners[T]?.add(listener);
-    return listener;
   }
 
   void unsubscribe<T>(Function listener) {
-    if (_listeners.containsKey(T)) {
-      _listeners[T]?.remove(listener);
+    try {
+      if (_listeners.containsKey(T)) {
+        _listeners[T]?.remove(listener);
+      }
+    } on Exception catch (e) {
+      print('Error unsubscribing from event: $e');
+
+      rethrow;
     }
   }
 }
